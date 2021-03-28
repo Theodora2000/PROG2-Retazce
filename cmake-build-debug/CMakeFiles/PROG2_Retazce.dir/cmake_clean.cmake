7b9fe431badcdbf32fd0a8738d627b4b22c13908
Theodora2000 <xsimont@stuba.sk>
2021-03-28 08:36:17 +0200

    begin

---
+++ cmake-build-debug/CMakeFiles/PROG2_Retazce.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROG2_Retazce.dir/main.c.obj"
+  "PROG2_Retazce.exe"
+  "PROG2_Retazce.exe.manifest"
+  "PROG2_Retazce.pdb"
+  "libPROG2_Retazce.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PROG2_Retazce.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
