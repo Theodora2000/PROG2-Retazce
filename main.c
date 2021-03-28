d289b95f6c76383e68de9c158c689a92c82e1d55
Theodora2000 <xsimont@stuba.sk>
2021-03-28 09:35:47 +0200

    Parametre príkazového riadku

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+
         int i;
 
         for (i = 0; i < argc; i++)
             printf("argv[%i]: %s\n", i, argv[i]);
 
-        return 0;
-    }
+
+
     return 0;
 }
