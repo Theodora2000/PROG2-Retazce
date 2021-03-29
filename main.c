554b86116d72ec0b25a2db4f796d6be6affb4e56
Theodora2000 <xsimont@stuba.sk>
2021-03-29 17:49:18 +0200

    jebena uloha

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-
-        int i;
-
-        for (i = 0; i < argc; i++)
-            printf("argv[%i]: %s\n", i, argv[i]);
-
-
+#include<stdio.h>
+int main()
+{
+    char a[10];
+    int i=0;
+    while( (a[i++]=getchar()) != '\n' && i < 10) /* take input from user until it's a newline or equal to 10 */
+        ;
+    a[i] = '\0'; /* null-terminate the string */
+    i = 0;
+    while(a[i] != '\0') /* print until we've hit \0 */
+        printf("%c",a[i++]);
 
     return 0;
 }
