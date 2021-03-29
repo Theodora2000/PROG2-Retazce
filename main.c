2ffe347b2c052025f4182f9e8c7f308513de495c
Theodora2000 <xsimont@stuba.sk>
2021-03-29 18:16:22 +0200

    strlen

--- main.c
+++ main.c
@@ -1,14 +1,7 @@
 #include<stdio.h>
+#include <string.h>
 int main()
 {
-    char a[10];
-    int i=0;
-    while( (a[i++]=getchar()) != '\n' && i < 10) /* take input from user until it's a newline or equal to 10 */
-        ;
-    a[i] = '\0'; /* null-terminate the string */
-    i = 0;
-    while(a[i] != '\0') /* print until we've hit \0 */
-        printf("%c",a[i++]);
-
-    return 0;
+    char str2[] = "home";
+    printf("%d", strlen(str2));
 }
