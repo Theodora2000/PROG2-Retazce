2fda3c690be01bf20dbbc469b92c8faa1078262d
Theodora2000 <xsimont@stuba.sk>
2021-03-29 18:19:22 +0200

    strcmp

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 #include <string.h>
 int main()
 {
-    char str2[] = "home";
-    printf("%d", strlen(str2));
+    char str1[] = "home";
+    char str2[] = "sweet";
+    if(strcmp(str1,str2)>0){
+        return 1;
+
+    } else if(strcmp(str1,str2)<0){
+        return -1;
+    }else{
+        return 0;
+    }
 }
