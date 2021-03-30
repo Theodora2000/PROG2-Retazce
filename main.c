82c50dfeb63418d9bfef7435a7369e0461778a8c
Theodora2000 <xsimont@stuba.sk>
2021-03-30 18:04:45 +0200

    Ďalšie príklady na reťazce 9

--- main.c
+++ main.c
@@ -3,115 +3,45 @@
 #include <string.h>
 #define MAX 10
 
-int strCompare(char* str1, char* str2)
+int findChar(char* recenica, char c)
 {
-    int index=0;
-    while(1>0)
-    {
-        if(str1[index]=='\0' && str2[index]=='\0')
-        {
-            return 0;
-        }
-        if(str1[index]!='\0' && str2[index]=='\0')
-        {
-            return 1;
-        }
-        if(str1[index]=='\0' && str2[index]!='\0')
-        {
-            return -1;
-        }
-
-        if(str1[index]<str2[index]){
-            return -1;
-        }
-        if(str1[index]>str2[index]){
-            return 1;
-        }
-        index++;
-
-    }
-}
-
-void Upisuj(char* string)
-{
-    int i;
-    char c;
-    for( i=0;i<MAX;i++)
-    {
-        c = (char)getchar();
-        if(c=='\n')
-        {
-
-            break;
-        }
-        fflush(stdin);
-        string[i]=c;
-    }
-    string[i]='\0';
-
-}
-
-int* findNum(int* niz,int n,int br)
-{
+    char* setaj;
+    setaj=recenica;
     int i=0;
-    int* setaj;
-    setaj=niz;
-    while(i<n)
+    while(*setaj!='\0')
     {
-        if(*setaj==br)
+        if(*setaj==c)
         {
-            return setaj;
+
+            return i;
         }
         setaj++;
         i++;
     }
-    return  NULL;
-
-}
-
-char* findChar(char* recenica, char c)
-{
-    char* setaj;
-    setaj=recenica;
-    int i=0;
-    while(*setaj!='\0')
-    {
-        if(*setaj==c)
-        {
-
-            return setaj;
-        }
-        setaj++;
-    }
-    return  NULL;
-}
-char* findCharLast(char* recenica, char c)
-{
-    char* setaj;
-    setaj=recenica;
-    char* returnVal;
-    returnVal=NULL;
-    int i=0;
-    while(*setaj!='\0')
-    {
-        if(*setaj==c)
-        {
-            returnVal=setaj;
-        }
-        setaj++;
-    }
-    return  returnVal;
+    return  -1;
 }
 
 int main () {
 
-    char c='a';
-    char string[]="anabela";
+    char* string = malloc(sizeof(char)*6);
+    strcpy(string,"*-*-*\0");
+    int poz;
 
-    for(int i=0;i<strlen(string);i++){
-        if(string[i]==c){
-            printf("%d ",i);
+    while((poz=findChar(string,'*'))!=-1)
+    {
+
+        int staro=strlen(string);
+        string=realloc(string,strlen(string)+2);
+        for(int i=staro-1; i>=poz; i--)
+        {
+            string[i+2]=string[i];
         }
+        string[poz]='A';
+        string[poz+1]='B';
+        string[poz+2]='C';
+        string[staro+2]='\0';
     }
+    printf("%s",string);
+
     return 0;
 }
