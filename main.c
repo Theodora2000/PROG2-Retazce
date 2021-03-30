33124a2481dbd850a1c55b593ea9e3f1d4931e25
Theodora2000 <xsimont@stuba.sk>
2021-03-30 18:24:51 +0200

    Ďalšie príklady na reťazce 4

--- main.c
+++ main.c
@@ -1,47 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 10
+#define N 10
 
-int findChar(char* recenica, char c)
+void new_string(char* in, int n, char* out)
 {
-    char* setaj;
-    setaj=recenica;
-    int i=0;
-    while(*setaj!='\0')
+    int i;
+    for( i=0;i<n-1;i++)
     {
-        if(*setaj==c)
-        {
 
-            return i;
-        }
-        setaj++;
-        i++;
+        out[i]=in[rand() % n];
     }
-    return  -1;
+    out[i]='\0';
+
 }
+
 
 int main () {
 
-    char* string = malloc(sizeof(char)*6);
-    strcpy(string,"*-*-*\0");
-    int poz;
-
-    while((poz=findChar(string,'*'))!=-1)
-    {
-
-        int staro=strlen(string);
-        string=realloc(string,strlen(string)+2);
-        for(int i=staro-1; i>=poz; i--)
-        {
-            string[i+2]=string[i];
-        }
-        string[poz]='A';
-        string[poz+1]='B';
-        string[poz+2]='C';
-        string[staro+2]='\0';
-    }
-    printf("%s",string);
+    char * in="programiranje";
+    char* out[N];
+    new_string(in,N,out);
+    printf("%s", out);
 
     return 0;
 }
